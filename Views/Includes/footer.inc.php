e168870710464dd76cc3f3e59ff8236ba4cdc562
HowBaka <gravy.thom@gmail.com>
2024-09-06 21:44:31 +0200

    [IMPROVES] Footer V1

---
+++ Views/Includes/footer.inc.php
@@ -0,0 +1,19 @@
+<footer class="p-8 border-t-2 border-zinc-950/5">
+    <div class="flex flex-col-reverse sm:flex-row items-center gap-2 justify-between w-full">
+        <span class="text-sm">© <?php echo date("Y") ?> Landingify, Inc. All rights reserved.</span>
+        <div class="flex space-x-4">
+            <!-- Icône Facebook -->
+            <a href="https://facebook.com" target="_blank" aria-label="Facebook">
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/></svg>
+            </a>
+            <!-- Icône Instagram -->
+            <a href="https://instagram.com" target="_blank" aria-label="Instagram">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line></svg>
+            </a>
+            <!-- Icône X (Twitter) -->
+            <a href="https://twitter.com" target="_blank" aria-label="X (Twitter)">
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+            </a>
+        </div>
+    </div>
+</footer>
